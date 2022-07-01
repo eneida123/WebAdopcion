583b9531eb34358dae85a4c84eb8615699cee236
EmanuelG0895 <66133406+EmanuelG0895@users.noreply.github.com>
2022-06-30 20:14:46 -0500

    actualizacion general

---
+++ adopcion/src/componentes/formularioAdopcion/formulario.jsx
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react';
+const formulario = () => {
+    return (  
+        <Fragment>
+            <h3>mi componente</h3>
+        </Fragment>
+    );
+}
+ 
+export default formulario;
